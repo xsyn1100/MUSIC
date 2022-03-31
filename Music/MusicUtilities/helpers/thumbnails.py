f8ac1c68fa09e785a655b7fb70c0fb569d50d80c
Doni Ramadhan <98083245+xsyn1100@users.noreply.github.com>
2022-03-31 12:03:16 +0700

    Update thumbnails.py

--- Music/MusicUtilities/helpers/thumbnails.py
+++ Music/MusicUtilities/helpers/thumbnails.py
@@ -1,9 +1,10 @@
 import os
-
-import aiofiles
+from os import path
 import aiohttp
-from PIL import Image, ImageDraw, ImageFont
-
+import aiofiles
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
 
 def changeImageSize(maxWidth, maxHeight, image):
     widthRatio = maxWidth / image.size[0]
@@ -22,7 +23,7 @@ async def gen_thumb(thumbnail, title, userid, theme, ctitle):
                 await f.write(await resp.read())
                 await f.close()
     image1 = Image.open(f"search/thumb{userid}.png")
-    image2 = Image.open(f"etc/{theme}.png")
+    image2 = Image.open(f"cache/{theme}.png")
     image3 = changeImageSize(1280, 720, image1)
     image4 = changeImageSize(1280, 720, image2)
     image5 = image3.convert("RGBA")
@@ -30,29 +31,16 @@ async def gen_thumb(thumbnail, title, userid, theme, ctitle):
     Image.alpha_composite(image5, image6).save(f"search/temp{userid}.png")
     img = Image.open(f"search/temp{userid}.png")
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype("cache/Roboto-Regular.ttf", 60)
-    font2 = ImageFont.truetype("cache/finalfont.ttf", 70)
-    draw.text(
-        (10, 630),
-        f"{title[:25]}...",
-        fill="white",
-        stroke_width=1,
-        stroke_fill="white",
-        font=font2,
-    )
-    draw.text(
-        (10, 550),
-        f"Memutar Di: {ctitle[:15]}...",
-        fill="white",
-        stroke_width=1,
-        stroke_fill="white",
-        font=font,
-    )
+    font = ImageFont.truetype("cache/finalfont.ttf", 55)
+    font2 = ImageFont.truetype("cache/finalfont.ttf", 75)
+    draw.text((25, 528), f"Playing on: {ctitle[:17]}...", fill="black", stroke_width = 1, stroke_fill="black" ,font=font)
+    draw.text((25, 610), f"{title[:20]}...", fill= "black", stroke_width = 1, stroke_fill="black", font=font2)
     img.save(f"search/final{userid}.png")
     os.remove(f"search/temp{userid}.png")
-    os.remove(f"search/thumb{userid}.png")
+    os.remove(f"search/thumb{userid}.png") 
     final = f"search/final{userid}.png"
     return final
+
 
 
 async def down_thumb(thumbnail, userid):
