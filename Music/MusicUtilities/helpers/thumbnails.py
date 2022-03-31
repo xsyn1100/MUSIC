d1b5f117736b47f2e13b4b84c7611ddc8cfaf158
Doni Ramadhan <98083245+xsyn1100@users.noreply.github.com>
2022-03-31 11:34:32 +0700

    Update thumbnails.py

--- Music/MusicUtilities/helpers/thumbnails.py
+++ Music/MusicUtilities/helpers/thumbnails.py
@@ -1,8 +1,10 @@
 import os
-
-import aiofiles
+from os import path
 import aiohttp
-from PIL import Image, ImageDraw, ImageFont
+import aiofiles
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
 
 
 def changeImageSize(maxWidth, maxHeight, image):
@@ -18,11 +20,11 @@ async def gen_thumb(thumbnail, title, userid, theme, ctitle):
     async with aiohttp.ClientSession() as session:
         async with session.get(thumbnail) as resp:
             if resp.status == 200:
-                f = await aiofiles.open(f"search/thumb{userid}.png", mode="wb")
+                f = await aiofiles.open(f"search/thumb{userid}.jpg", mode="wb")
                 await f.write(await resp.read())
                 await f.close()
-    image1 = Image.open(f"search/thumb{userid}.png")
-    image2 = Image.open(f"etc/{theme}.png")
+    image1 = Image.open(f"search/thumb{userid}.jpg")
+    image2 = Image.open(f"cache/{theme}.png")
     image3 = changeImageSize(1280, 720, image1)
     image4 = changeImageSize(1280, 720, image2)
     image5 = image3.convert("RGBA")
@@ -30,27 +32,13 @@ async def gen_thumb(thumbnail, title, userid, theme, ctitle):
     Image.alpha_composite(image5, image6).save(f"search/temp{userid}.png")
     img = Image.open(f"search/temp{userid}.png")
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype("cache/Roboto-Regular.ttf", 60)
-    font2 = ImageFont.truetype("cache/finalfont.ttf", 70)
-    draw.text(
-        (10, 630),
-        f"{title[:25]}...",
-        fill="white",
-        stroke_width=1,
-        stroke_fill="white",
-        font=font2,
-    )
-    draw.text(
-        (10, 550),
-        f"Playing On: {ctitle[:15]}...",
-        fill="white",
-        stroke_width=1,
-        stroke_fill="white",
-        font=font,
-    )
+    font = ImageFont.truetype("cache/regular.ttf", 50)
+    font2 = ImageFont.truetype("cache/medium.ttf", 72)
+    draw.text((27, 543), f"Playing on {ctitle[:12]}", fill="black", font=font)
+    draw.text((25, 615), f"{title[:20]}...", fill= "black", font=font2)
     img.save(f"search/final{userid}.png")
     os.remove(f"search/temp{userid}.png")
-    os.remove(f"search/thumb{userid}.png")
+    os.remove(f"search/thumb{userid}.jpg") 
     final = f"search/final{userid}.png"
     return final
 
@@ -59,8 +47,8 @@ async def down_thumb(thumbnail, userid):
     async with aiohttp.ClientSession() as session:
         async with session.get(thumbnail) as resp:
             if resp.status == 200:
-                f = await aiofiles.open(f"search/thumb{userid}.png", mode="wb")
+                f = await aiofiles.open(f"search/thumb{userid}.jpg", mode="wb")
                 await f.write(await resp.read())
                 await f.close()
-    final = f"search/thumb{userid}.png"
+    final = f"search/thumb{userid}.jpg"
     return final
